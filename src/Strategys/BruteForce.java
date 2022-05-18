c0fb04ccb23f558dd9bd3cd5779e52cad308dd3e
Your Name <you@example.com>
2022-05-17 21:54:17 -0300

    completed BruteForce

--- src/Strategys/BruteForce.java
+++ src/Strategys/BruteForce.java
@@ -8,18 +8,23 @@ import java.util.Map;
 public class BruteForce implements Strategy{
     @Override
     public void solv(Cases cases) {
-        for (int i = 0; i <=3; i++){
-            cases.results[cases.matchesQty-1] = i;
-            if (i == 3){
-                for (int j = cases.matchesQty-1; j>=0; j--){
-                    if (j == 3){
-                        cases.results[j-1]++;
-                        cases.results[j] = 0;
+        boolean notFound = true;
+        while (notFound){
+            for (int i = 0; i <=3; i++) {
+                cases.results[cases.matchesQty - 1] = i;
+                if (i == 3) {
+                    for (int j = cases.matchesQty - 1; j > 0; j--) {
+                        if (cases.results[j] == 3) {
+                            cases.results[j - 1]++;
+                            cases.results[j] = 0;
+                        }
                     }
                 }
+                if (check(cases)) {
+                    notFound = false;
+                    break;
+                }
             }
-           if (check(cases))
-               break;
         }
     }
 
@@ -30,14 +35,14 @@ public class BruteForce implements Strategy{
 
         for (int i = 0; i<cases.matchesQty; i++){
             if (cases.results[i] == 0){
-                toCheckPoints.put(cases.matches[i][0], +1);
-                toCheckPoints.put(cases.matches[i][1], +1);
+                toCheckPoints.put(cases.matches[i][0], toCheckPoints.get(cases.matches[i][0])+1);
+                toCheckPoints.put(cases.matches[i][1], toCheckPoints.get(cases.matches[i][1])+1);
             }
             else if (cases.results[i] == 1){
-                toCheckPoints.put(cases.matches[i][0], +3);
+                toCheckPoints.put(cases.matches[i][0], toCheckPoints.get(cases.matches[i][0])+3);
             }
             else {
-                toCheckPoints.put(cases.matches[i][1], +3);
+                toCheckPoints.put(cases.matches[i][1], toCheckPoints.get(cases.matches[i][1])+3);
             }
         }
 
