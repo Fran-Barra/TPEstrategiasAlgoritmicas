79555f43c28f1d9ac7789844f63dfdc404ffae5e
Your Name <you@example.com>
2022-05-20 18:28:47 -0300

    optimizado back trac

--- src/Strategys/BackTrackingF.java
+++ src/Strategys/BackTrackingF.java
@@ -37,38 +37,35 @@ public class BackTrackingF implements Strategy{
         for (String team : cases.teams)
             toCheckPoints.put(team, 0);
 
-        for (int j = 0; j < cases.matchesQty; j++) {
-            if (j == i) {
-                if (k == 0) {
-                    toCheckPoints.put(cases.matches[j][0], toCheckPoints.get(cases.matches[j][0]) + 1);
-                    toCheckPoints.put(cases.matches[j][1], toCheckPoints.get(cases.matches[j][1]) + 1);
-                } else if (k == 1) {
-                    toCheckPoints.put(cases.matches[j][0], toCheckPoints.get(cases.matches[j][0]) + 3);
-                } else if (k == 2){
-                    toCheckPoints.put(cases.matches[j][1], toCheckPoints.get(cases.matches[j][1]) + 3);
-                }
-
-            } else {
-                if (cases.results[j] == 0) {
-                    toCheckPoints.put(cases.matches[j][0], toCheckPoints.get(cases.matches[j][0]) + 1);
-                    toCheckPoints.put(cases.matches[j][1], toCheckPoints.get(cases.matches[j][1]) + 1);
-                } else if (cases.results[j] == 1) {
-                    toCheckPoints.put(cases.matches[j][0], toCheckPoints.get(cases.matches[j][0]) + 3);
-                } else if (cases.results[j] == 2){
-                    toCheckPoints.put(cases.matches[j][1], toCheckPoints.get(cases.matches[j][1]) + 3);
-                }
+        for (int j = 0; j < i; j++) {
+            if (cases.results[j] == 0) {
+                toCheckPoints.put(cases.matches[j][0], toCheckPoints.get(cases.matches[j][0]) + 1);
+                toCheckPoints.put(cases.matches[j][1], toCheckPoints.get(cases.matches[j][1]) + 1);
+            } else if (cases.results[j] == 1) {
+                toCheckPoints.put(cases.matches[j][0], toCheckPoints.get(cases.matches[j][0]) + 3);
+            } else if (cases.results[j] == 2){
+                toCheckPoints.put(cases.matches[j][1], toCheckPoints.get(cases.matches[j][1]) + 3);
             }
 
         }
-        for (String team : cases.teams){
-            if (i == cases.matchesQty - 1) {
+        if (k == 0) {
+            toCheckPoints.put(cases.matches[i][0], toCheckPoints.get(cases.matches[i][0]) + 1);
+            toCheckPoints.put(cases.matches[i][1], toCheckPoints.get(cases.matches[i][1]) + 1);
+        } else if (k == 1) {
+            toCheckPoints.put(cases.matches[i][0], toCheckPoints.get(cases.matches[i][0]) + 3);
+        } else if (k == 2){
+            toCheckPoints.put(cases.matches[i][1], toCheckPoints.get(cases.matches[i][1]) + 3);
+        }
+        if (i == cases.matchesQty - 1){
+            for (String team : cases.teams)
                 if (!Objects.equals(toCheckPoints.get(team), cases.points.get(team)))
                     return false;
-            }else{
+        } else {
+            for (String team : cases.teams)
                 if (toCheckPoints.get(team) > cases.points.get(team))
                     return false;
-            }
         }
+
         return true;
     }
 
